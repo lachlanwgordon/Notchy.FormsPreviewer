df80b65a8321dcea096d0d8d3fb76358c3c062de
Lachlan Gordon <lachlan@cookingthebooks.com.au>
2019-07-04 10:56:17 +1000

    Added a sample app using the Shell App template

---
+++ Notchy.FormsPreviewer/Notchy.FormsPreviewer.Sample/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Notchy.FormsPreviewer.Sample.Models;
+
+namespace Notchy.FormsPreviewer.Sample.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
